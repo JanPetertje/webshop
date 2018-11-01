6ed8691bd5f33af430a5d804d37be4c71f3d1e81
nijlanderrr <rens.development@gmail.com>
2018-11-01 11:21:32 +0100

    Menu onderdeel gemaakt. Kan geinclude worden.

---
+++ inc/parts/menu.php
@@ -0,0 +1,17 @@
+<div class="menu">
+    <div class="menu-content">
+        <a href="index.php">
+            <h1><span>World</span> <span>Wide</span> Importers</h1>
+        </a>
+        <ul>
+            <li>
+                <input type="text" name="search_input">
+            </li>
+            <li>
+                <a href="cart.php">
+                    <img src="img/shopping-cart.png" alt="shopping-cart">
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
---
+++ views/login.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Login</title>
+        <link rel="stylesheet" href="inc/css/fonts.css">
+        <link rel="stylesheet" href="inc/css/main.css">
+        <link rel="stylesheet" href="inc/css/login.css">
+    </head>
+    <body>
+    <?php
+
+    include "inc/parts/menu.php";
+
+    ?>
+    </body>
+</html>
