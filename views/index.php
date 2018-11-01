46e3e1e9af84c250682d72d06162553f33991cc6
patrickdejong <patrickjong123@gmail.com>
2018-11-01 12:07:23 +0100

    allahu ackbar

---
+++ views/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Eerste wereldoorlog</title>
+        <link rel="stylesheet" href="inc/css/fonts.css">
+        <link rel="stylesheet" href="inc/css/main.css">
+    </head>
+    <body>
+    <?php
+
+    include "inc/parts/menu.php";
+
+    ?>
+    <div>
+        <h1>Popular items</h1>
+    </div>
+    </body>
+</html>
