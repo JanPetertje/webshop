932f28528ae6afa08a949f188c41b3195fbab545
patrickdejong <patrickjong123@gmail.com>
2018-11-02 09:03:54 +0100

    allahu ackbar

--- views/index.php
+++ views/index.php
@@ -5,18 +5,32 @@
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Eerste wereldoorlog</title>
+        <title>WWI</title>
         <link rel="stylesheet" href="inc/css/fonts.css">
         <link rel="stylesheet" href="inc/css/main.css">
+        <link rel="stylesheet" href="inc/css/index.css">
     </head>
+
     <body>
+
     <?php
-
     include "inc/parts/menu.php";
-
     ?>
+
     <div>
         <h1>Popular items</h1>
     </div>
+
+    <div>
+        <ul>
+            <li>Chocolate</li>
+            <li>USB Products</li>
+            <li>Mugs</li>
+            <li>Toys</li>
+            <li>Clothing</li>
+            <li>Shipping Products</li>
+        </ul>
+    </div>
+
     </body>
 </html>
