618ea1db3e58d561c5ca8a602902eac9fb351dfe
jan-peter <jppoolen@gmail.com>
2018-11-02 10:23:39 +0100

    Product groups

---
+++ views/ProductGroups.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="inc/css/fonts.css">
+    <link rel="stylesheet" href="inc/css/main.css">
+    <link rel="stylesheet" href="inc/css/winkelwagen.css">
+</head>
+
+<body>
+
+<?php
+include "inc/parts/menu.php";
+?>
+
+<div class="top">
+    <p class="titel">Winkelwagen</p>
+</div>
+
+<div class="kopjes">
+    <h1 class="kopje1">Product</h1>
+    <h2 class="item">item</h2>
+    <h3 class="quantity">price</h3>
+    <h4 class="price">price</h4>
+    <!--maak voor elke header een 1/8,3/8,5/8,7/8 zodat alles netjes te staan komt op de pagina-->
+</div>
+
+<a href="#" class="myButton">order now!</a>
+
+
+
+
+</body>
+</html>
