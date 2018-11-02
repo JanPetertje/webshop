1e710f646de79866b45855e6a60ce1471a79b30f
jan-peter <jppoolen@gmail.com>
2018-11-02 11:02:23 +0100

    Product groups

--- views/ProductGroups.php
+++ views/ProductGroups.php
@@ -5,10 +5,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>WWI | ProductGroups</title>
     <link rel="stylesheet" href="inc/css/fonts.css">
     <link rel="stylesheet" href="inc/css/main.css">
     <link rel="stylesheet" href="inc/css/winkelwagen.css">
+    <link rel="stylesheet" href="inc/css/ProductGroupsStyles.css">
 </head>
 
 <body>
@@ -18,18 +19,13 @@ include "inc/parts/menu.php";
 ?>
 
 <div class="top">
-    <p class="titel">Winkelwagen</p>
+    <p class="titel">Product Groups</p>
 </div>
 
-<div class="kopjes">
-    <h1 class="kopje1">Product</h1>
-    <h2 class="item">item</h2>
-    <h3 class="quantity">price</h3>
-    <h4 class="price">price</h4>
-    <!--maak voor elke header een 1/8,3/8,5/8,7/8 zodat alles netjes te staan komt op de pagina-->
+<div class="Group">
+    <img src="img/chocoreep.jpg" alt="chocoreep">
+    <p Title</p>
 </div>
-
-<a href="#" class="myButton">order now!</a>
 
 
 
