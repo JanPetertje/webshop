d7b4917b7f0c5ec58fd6d94f4bda16c1e70b6b48
LivingPSYCHO <maxbrouwer1999@gmail.com>
2018-11-06 13:54:17 +0100

    Login.php verder uitgewerkt door niet mij. PLZ NO CRASHERINO

--- login.php
+++ login.php
@@ -5,5 +5,17 @@ include "views/login.php";
 
 if(isset($_POST["login"])) {
     $username = trim($_POST["username"]);
-    $password = trim($_POST["password"]);
+    $password = hash("sha256", trim($_POST["password"]));
+
+    $stmt = $conn->prepare("SELECT COUNT(account_id) FROM accounts WHERE username = :user AND password = :psw");
+
+    $stmt->bindParam(":user", $username);
+    $stmt->bindParam(":psw", $password);
+
+    if($stmt->execute()) {
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        print_r($result);
+
+
+    }
 }
