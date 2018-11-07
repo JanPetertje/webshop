19148fc404eb3cb2ac6eca6783798387745e092a
patrickdejong <patrickjong123@gmail.com>
2018-11-07 12:20:44 +0100

    allahu ackbar

--- views/searchresults.php
+++ views/searchresults.php
@@ -8,11 +8,35 @@
         ?>
     </head>
     <body>
-    <h1>
+    <div>
+       <h1>
     <?php
-    $searchinput = $_GET;
-    print "You were searching for: " . $searchinput["search_input"];
-    ?>
+        $searchinput = $_GET;
+        $searchinput = $searchinput["search_input"];
+        print "Results for: ".$searchinput;
+        ?>
     </h1>
+    </div>
+
+    <div style="background-color: black; height: 100%; color: white;">
+        <?php
+        $products = array("hats", "cups", "dog");
+        $x = 0;
+        foreach ($products as $key => $product) {
+            if ($product == $searchinput) {
+                print "<br>Results found!";
+                $x = 1;
+                return;
+            } elseif (empty($searchinput)) {
+                print "No input - error";
+                $x = 1;
+                return;
+            }
+        }
+        if ($x == 0) {
+            print "No results for $searchinput";
+        }
+        ?>
+    </div>
     </body>
 </html>
