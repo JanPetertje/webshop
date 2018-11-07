6684b0e562389adc65fe3209f808499f52995782
LivingPSYCHO <maxbrouwer1999@gmail.com>
2018-11-07 12:27:06 +0100

    ccs file voor de product pagina. PLZ NO CRASHERINO

--- views/index.php
+++ views/index.php
@@ -25,18 +25,17 @@
     <div>
         <ul class="list">
             <br><a class="productgroupLink" href="ProductGroups.php">Product Groups  </a><br><br>
-            <li><a class="productgroupLink" href="product.php">Novelty Items</a></li>
-            <li><a class="productgroupLink" href="product.php">Clothing</a></li>
-            <li><a class="productgroupLink" href="product.php">Mugs</a></li>
-            <li><a class="productgroupLink" href="product.php">T-Shirts</a></li>
-            <li><a class="productgroupLink" href="product.php">Clothing</a></li>
-            <li><a class="productgroupLink" href="product.php">Airline Novelties</a></li>
-            <li><a class="productgroupLink" href="product.php">Computing Novelties</a></li>
-            <li><a class="productgroupLink" href="product.php">USB Novelties</a></li>
-            <li><a class="productgroupLink" href="product.php">Furry Footwear</a></li>
-            <li><a class="productgroupLink" href="product.php">Toys</a></li>
-            <li><a class="productgroupLink" href="product.php">Packaging Materials</Materials></a></li>
-            <li><a class="productgroupLink" href="product.php">Shipping Products</a></li><br>
+            <li><a class="productgroupLink" href="product.php?name=Novelty%20Items">Novelty Items</a></li>
+            <li><a class="productgroupLink" href="product.php?name=Clothing">Clothing</a></li>
+            <li><a class="productgroupLink" href="product.php?name=Mugs">Mugs</a></li>
+            <li><a class="productgroupLink" href="product.php?name=T-Shirts">T-Shirts</a></li>
+            <li><a class="productgroupLink" href="product.php?name=Airline%20Novelties">Airline Novelties</a></li>
+            <li><a class="productgroupLink" href="product.php?name=Computing%20Novelties">Computing Novelties</a></li>
+            <li><a class="productgroupLink" href="product.php?name=USB%20Novelties">USB Novelties</a></li>
+            <li><a class="productgroupLink" href="product.php?name=Furry%20Footwear">Furry Footwear</a></li>
+            <li><a class="productgroupLink" href="product.php?name=Toys">Toys</a></li>
+            <li><a class="productgroupLink" href="product.php?name=Packaging%20Materials">Packaging Materials</Materials></a></li>
+            <li><a class="productgroupLink" href="product.php?name=Shipping%20Products">Shipping Products</a></li><br>
         </ul>
     </div>
 
--- views/product.php
+++ views/product.php
@@ -3,13 +3,35 @@
     <head>
         <link rel="stylesheet" href="inc/css/fonts.css">
         <link rel="stylesheet" href="inc/css/main.css">
+        <link rel="stylesheet" href="inc/css/product.css">
     </head>
 
     <body>
     <?php
+    $name = ($_GET["name"])
+    ?>
+    <?php
     include "inc/parts/menu.php";
     ?>
-    <h1>DIT IS ZON PRODUCT PAGINAATJE</h1>
+
+    <div class="top">
+        <p class="titel"><?php echo $name ?></p>
+    </div>
+    <ul class="list">
+        <br><a class="productgroupLink" href="ProductGroups.php">Product Groups  </a><br><br>
+        <li><a class="productgroupLink" href="product.php?name=Novelty%20Items">Novelty Items</a></li>
+        <li><a class="productgroupLink" href="product.php?name=Clothing">Clothing</a></li>
+        <li><a class="productgroupLink" href="product.php?name=Mugs">Mugs</a></li>
+        <li><a class="productgroupLink" href="product.php?name=T-Shirts">T-Shirts</a></li>
+        <li><a class="productgroupLink" href="product.php?name=Airline%20Novelties">Airline Novelties</a></li>
+        <li><a class="productgroupLink" href="product.php?name=Computing%20Novelties">Computing Novelties</a></li>
+        <li><a class="productgroupLink" href="product.php?name=USB%20Novelties">USB Novelties</a></li>
+        <li><a class="productgroupLink" href="product.php?name=Furry%20Footwear">Furry Footwear</a></li>
+        <li><a class="productgroupLink" href="product.php?name=Toys">Toys</a></li>
+        <li><a class="productgroupLink" href="product.php?name=Packaging%20Materials">Packaging Materials</Materials></a></li>
+        <li><a class="productgroupLink" href="product.php?name=Shipping%20Products">Shipping Products</a></li><br>
+    </ul>
+
     </body>
 </html>
 
