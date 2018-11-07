3278dd24028ca97d36e2a3d3bcfe9cd5fce0f4be
patrickdejong <patrickjong123@gmail.com>
2018-11-07 10:34:06 +0100

    allahu ackbar

--- views/searchresults.php
+++ views/searchresults.php
@@ -8,7 +8,11 @@
         ?>
     </head>
     <body>
-    <h1>NO RESULTS FOUND HAHAH WANT ER IS GEEN PRODUCT AANWEZIG XDDDDDD</h1>
-    <embed url="https://i.ytimg.com/an_webp/GA6NN6yTyow/mqdefault_6s.webp?du=3000&sqp=CIzMit8F&rs=AOn4CLCAKrqL7anMxWHgelg2xyskAL0nUA" width="600" height="400" scale="aspect" controller="true">
+    <h1>
+    <?php
+    $searchinput = $_GET;
+    print "You were searching for: " . $searchinput["search_input"];
+    ?>
+    </h1>
     </body>
 </html>
