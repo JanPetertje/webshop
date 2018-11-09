256c2ad3d9bde4653fa633dd9253f550645f745d
patrickdejong <patrickjong123@gmail.com>
2018-11-09 12:49:04 +0100

    allahu ackbar

--- views/index.php
+++ views/index.php
@@ -23,28 +23,19 @@
         <h1>Popular items</h1>
     </div>
 
-    <div>
-        $productgroups = $conn->prepare("SELECT StockGroupName FROM stockgroups");
-        $productgroups->execute();
-        while ($row = $productgroups->fetch()) {
-        $groupnames = $row["StockGroupName"];
-        print ("<li><a class='productgroupLink' href='product.php?name=Novelty%20Items'> $groupnames</a></li>");
-        }
-    </div>
+
     <div>
         <ul class="list">
             <br><a class="productgroupLink" href="ProductGroups.php">Product Groups  </a><br><br>
-            <li><a class="productgroupLink" href="product.php?name=Novelty%20Items">Novelty Items</a></li>
-            <li><a class="productgroupLink" href="product.php?name=Clothing">Clothing</a></li>
-            <li><a class="productgroupLink" href="product.php?name=Mugs">Mugs</a></li>
-            <li><a class="productgroupLink" href="product.php?name=T-Shirts">T-Shirts</a></li>
-            <li><a class="productgroupLink" href="product.php?name=Airline%20Novelties">Airline Novelties</a></li>
-            <li><a class="productgroupLink" href="product.php?name=Computing%20Novelties">Computing Novelties</a></li>
-            <li><a class="productgroupLink" href="product.php?name=USB%20Novelties">USB Novelties</a></li>
-            <li><a class="productgroupLink" href="product.php?name=Furry%20Footwear">Furry Footwear</a></li>
-            <li><a class="productgroupLink" href="product.php?name=Toys">Toys</a></li>
-            <li><a class="productgroupLink" href="product.php?name=Packaging%20Materials">Packaging Materials</Materials></a></li>
-            <li><a class="productgroupLink" href="product.php?name=Shipping%20Products">Shipping Products</a></li><br><br>
+            <?php
+            $productgroups = $conn->prepare("SELECT StockGroupName FROM stockgroups");
+            $productgroups->execute();
+            while ($row = $productgroups->fetch()) {
+                $groupnames = $row["StockGroupName"];
+                print ("<li><a class='productgroupLink' href='product.php?name=Novelty%20Items'> $groupnames</a></li>");
+            }
+            print"<br><br><br>";
+            ?>
         </ul>
     </div>
 
