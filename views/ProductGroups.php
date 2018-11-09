de69af6e8a60da13fe82fc478bb37024aac4a44d
jan-peter <jppoolen@gmail.com>
2018-11-09 10:41:50 +0100

    aangepast

--- views/ProductGroups.php
+++ views/ProductGroups.php
@@ -17,39 +17,46 @@
 include "inc/parts/menu.php";
 ?>
 
+
 <div class="top">
     <p class="titel">Product Groups</p>
 </div>
 
-<div>
-    <ul>
-        <li><a class="productgroupLink" href="views/ProductGroups.php">Product Groups</a></li>
-        <li><a class="productgroupLink">Chocolate</a></li>
-        <li><a class="productgroupLink">USB Products</a></li>
-        <li><a class="productgroupLink">Mugs</a></li>
-        <li><a class="productgroupLink">Toys</a></li>
-        <li><a class="productgroupLink">Clothing</a></li>
-        <li><a class="productgroupLink">Shipping Products</a></li>
-    </ul>
+<div class="grid-container">
+    <div class="grid-item"><p class="productgroupname group1">Novelty Items</p></div>
+    <div class="grid-item"><p class="productgroupname group2">Clothing</p></div>
+    <div class="grid-item">3</div>
+    <div class="grid-item">4</div>
+    <div class="grid-item">5</div>
+    <div class="grid-item">6</div>
+    <div class="grid-item">7</div>
+    <div class="grid-item">8</div>
+    <div class="grid-item">9</div>
 </div>
-<li class="webshop">
-<div class="container">
-    <div class="row">
-        <div class="col-lg-5 group1">
-            <img src= https://discountoffice.nl/productImages/8/large/Q891853-2.jpg class="foto">
-            <p class="productgroupname">Chocolate bar</p>
-        </div>
-    </div>
-</li>
+
+<!--<div class="container">-->
+<!--    <div class="row">-->
+<!--        <div class="col-lg-5 group1">-->
+<!--            <p class="productgroupname">Chocolate bar</p>-->
+<!--        </div>-->
+<!--        <div class="col-lg-5 group1">-->
+<!--            <p class="productgroupname">Chocolate bar</p>-->
+<!--        </div>-->
+<!---->
+<!--            <div class="col-lg-5 group1">-->
+<!--                <p class="productgroupname">Chocolate bar</p>-->
+<!--            </div>-->
+<!--    </div>-->
+<!---->
+<!--    <div class="row">-->
+<!--        <div class="col-lg-5 group1">-->
+<!--            <p class="productgroupname">Chocolate bar</p>-->
+<!--        </div>-->
+<!--        <div class="col-lg-5 group1">-->
+<!--            <p class="productgroupname">Chocolate bar</p>-->
+<!--        </div>-->
 
 
-<!--<div class="group1">-->
-<!--    <p class="productgroupname">Chocolate bar</p>-->
-<!--</div>-->
-
-<!--<div class="group1">-->
-<!--    <p class="productgroupname">Chocolate bar</p>-->
-<!--</div>-->
 
 
 
