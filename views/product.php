7a438f4a68682b7bcece6a906ec8ba35b105ab74
jessemolhoek <s1118630@student.windesheim.nl>
2018-11-09 13:37:23 +0100

    product php pakt andere groups nu uit de database inplaats van elly's

--- views/product.php
+++ views/product.php
@@ -12,6 +12,7 @@
     ?>
     <?php
     include "inc/parts/menu.php";
+    include "inc/parts/db.php";
     ?>
 
     <div class="top">
@@ -19,17 +20,28 @@
     </div>
     <ul class="list">
         <br><a class="productgroupLink" href="ProductGroups.php">Product Groups  </a><br><br>
-        <li><a class="productgroupLink" href="product.php?name=Novelty%20Items">Novelty Items</a></li>
-        <li><a class="productgroupLink" href="product.php?name=Clothing">Clothing</a></li>
-        <li><a class="productgroupLink" href="product.php?name=Mugs">Mugs</a></li>
-        <li><a class="productgroupLink" href="product.php?name=T-Shirts">T-Shirts</a></li>
-        <li><a class="productgroupLink" href="product.php?name=Airline%20Novelties">Airline Novelties</a></li>
-        <li><a class="productgroupLink" href="product.php?name=Computing%20Novelties">Computing Novelties</a></li>
-        <li><a class="productgroupLink" href="product.php?name=USB%20Novelties">USB Novelties</a></li>
-        <li><a class="productgroupLink" href="product.php?name=Furry%20Footwear">Furry Footwear</a></li>
-        <li><a class="productgroupLink" href="product.php?name=Toys">Toys</a></li>
-        <li><a class="productgroupLink" href="product.php?name=Packaging%20Materials">Packaging Materials</Materials></a></li>
-        <li><a class="productgroupLink" href="product.php?name=Shipping%20Products">Shipping Products</a></li><br>
+
+        <?php
+        $productgroups = $conn->prepare("SELECT StockGroupName FROM stockgroups");
+        $productgroups->execute();
+        while ($row = $productgroups->fetch()) {
+            $groupnames = $row["StockGroupName"];
+            print ("<li><a class='productgroupLink' href='product.php?name=Novelty%20Items'> $groupnames</a></li>");
+        }
+        print"<br><br><br>";
+        ?>
+
+<!--        <li><a class="productgroupLink" href="product.php?name=Novelty%20Items">Novelty Items</a></li>-->
+<!--        <li><a class="productgroupLink" href="product.php?name=Clothing">Clothing</a></li>-->
+<!--        <li><a class="productgroupLink" href="product.php?name=Mugs">Mugs</a></li>-->
+<!--        <li><a class="productgroupLink" href="product.php?name=T-Shirts">T-Shirts</a></li>-->
+<!--        <li><a class="productgroupLink" href="product.php?name=Airline%20Novelties">Airline Novelties</a></li>-->
+<!--        <li><a class="productgroupLink" href="product.php?name=Computing%20Novelties">Computing Novelties</a></li>-->
+<!--        <li><a class="productgroupLink" href="product.php?name=USB%20Novelties">USB Novelties</a></li>-->
+<!--        <li><a class="productgroupLink" href="product.php?name=Furry%20Footwear">Furry Footwear</a></li>-->
+<!--        <li><a class="productgroupLink" href="product.php?name=Toys">Toys</a></li>-->
+<!--        <li><a class="productgroupLink" href="product.php?name=Packaging%20Materials">Packaging Materials</Materials></a></li>-->
+<!--        <li><a class="productgroupLink" href="product.php?name=Shipping%20Products">Shipping Products</a></li><br>-->
     </ul>
 
     </body>
