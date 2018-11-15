1b901a5b608ca3baec7ee1d527087d5c2a54c2a0
jan-peter <jppoolen@gmail.com>
2018-11-15 10:49:32 +0100

    db toegevoegd

--- inc/parts/db.php
+++ inc/parts/db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "homo";
+$password = "homo";
 $dbname = "wideworldimporters";
 
 try {
--- views/ProductGroups.php
+++ views/ProductGroups.php
@@ -15,6 +15,7 @@
 
 <?php
 include "inc/parts/menu.php";
+include "inc/parts/db.php";
 ?>
 
 
@@ -26,36 +27,24 @@ include "inc/parts/menu.php";
     <div class="row">
         <div class="items">
 <?php
-            $productgroups = $conn->prepare("SELECT StockGroupName FROM stockgroups");
+                $productgroups = $pdo->prepare("SELECT StockGroupName FROM stockgroups");
+//            $productgroups = $pdo->prepare(state"SELECT StockGroupName FROM stockgroups");
             $productgroups->execute();
             while ($row = $productgroups->fetch()) {
                 $groupnames = $row["StockGroupName"];
                 echo '<div class="card product-card">
                       <img src="https://hlfppt.org/wp-content/uploads/2017/04/placeholder.png" alt="Product picture" class="card-img-top">
                       <div class="card-body">
-                      <h5 class="card-title">' . $groupnames . '/h5>
-                      <a class="btn btn-primary" href="productOverview.php?name=' . $groupnames . '>See more!</a>
+                      <h5 class="card-title">' . $groupnames . '</h5>
+                      <a class="btn btn-primary" href="productOverview.php?name=' . $groupnames . '">"See more!"</a>
                     </div>
                 </div>';
             }
-//<!--    <div>3</div>-->
-//<!--    <div>4</div>-->
-//<!--    <div>5</div>-->
-//<!--    <div>6</div>-->
-//<!--    <div>7</div>-->
-//<!--    <div>8</div>-->
-//<!--    <div>9</div>-->
+
 $pdo = NULL;
+
 ?>
-            <!--    <div class="grid-item"><a class="productgroupname group" href="https://www.w3schools.com">Novelty Items</a></div>-->
-<!--    <div class="grid-item"><p class="productgroupname group2">Clothing</p></div>-->
-<!--    <div class="grid-item">3</div>-->
-<!--    <div class="grid-item">4</div>-->
-<!--    <div class="grid-item">5</div>-->
-<!--    <div class="grid-item">6</div>-->
-<!--    <div class="grid-item">7</div>-->
-<!--    <div class="grid-item">8</div>-->
-<!--    <div class="grid-item">9</div>-->
+
         </div>
 </div>
 
