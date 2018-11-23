0a8477c118f7006009fef3df6838ae03535e352f
nijlanderrr <rens.development@gmail.com>
2018-11-23 10:35:41 +0100

    Register pagina aangepast.

---
+++ views/order.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php
+
+        include "inc/parts/head.php";
+
+        ?>
+        <link rel="stylesheet" href="inc/css/order.css">
+    </head>
+    <body>
+        <?php
+
+        include "inc/parts/menu.php";
+
+        ?>
+        <div class="container order-container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <form action="order.php" method="post">
+                        <div class="form-row">
+                            <div class="form-group col-lg-4">
+                                <label for="firstNameInput">First name</label>
+                                <input name="firstName" type="text" class="form-control" id="firstNameInput">
+                            </div>
+                            <div class="form-group col-lg-2">
+                                <label for="initialInput">Initials</label>
+                                <input name="initials" type="text" class="form-control" id="initialInput">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="lastNameInput">Last name</label>
+                                <input name="lastName" type="text" class="form-control" id="lastNameInput">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="emailInput">E-mail</label>
+                            <input name="email" type="email" class="form-control" id="emailInput" aria-describedby="emailHelp">
+                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-lg-3">
+                                <label for="postalInput">Zip code</label>
+                                <input name="postal" type="text" class="form-control" id="postalInput">
+                            </div>
+                            <div class="form-group col-lg-3">
+                                <label for="houseInput">House number</label>
+                                <input name="houseNumber" type="text" class="form-control" id="houseInput">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="cityInput">City</label>
+                                <input name="city" type="text" class="form-control" id="cityInput">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="countryInput">Country</label>
+                            <input name="country" type="text" id="countryInput" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="termsCheck">
+                                <label class="form-check-label" for="termsCheck">
+                                    I have read, understood and accept the General Terms and Conditions relevant to e-commerce.
+                                </label>
+                            </div>
+                        </div>
+                        <input value="Register" type="submit" class="btn btn-primary">
+                    </form>
+                </div>
+                <div class="col-lg-4">
+                    <div class="summary-container">
+                        <h1>Summary</h1>
+                        <ul>
+                            <li>
+                                <p>Shipping<span>€ 135</span></p>
+                            </li>
+                            <li>
+                                <p>Tax<span>€ 135</span></p>
+                            </li>
+                            <li>
+                                <p>Total<span>€ 135</span></p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php
+
+        include "inc/parts/footer.php";
+
+        ?>
+    </body>
+</html>
