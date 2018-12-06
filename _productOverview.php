e3aab9f162cc7acd7f38865c0bc7a93c6a2c0161
JesseMolhoek <jmolhoek12@gmail.com>
2018-12-06 14:37:15 +0100

    goed aantal producten

--- _productOverview.php
+++ _productOverview.php
@@ -2,7 +2,7 @@
 
 include "inc/parts/db.php";
 
-// Product groups for menu
+// Product groups
 
 $stmt = $conn->prepare("SELECT StockGroupName, StockGroupID FROM stockgroups");
 
@@ -26,24 +26,24 @@ if(isset($_GET["id"]) && !empty($_GET["id"])) {
             $category = $_GET["id"];
 
             if(isset($_GET["limit"]) && !empty($_GET["limit"]) && $_GET["limit"] == 0) {
-                $stmt = $conn->prepare("SELECT SG.StockGroupID, S.StockItemName, S.RecommendedRetailPrice, S.StockItemID FROM stockitems S JOIN stockitemstockgroups G ON S.StockItemID = G.StockItemID JOIN stockgroups SG ON G.StockGroupID = SG.StockGroupID WHERE G.StockGroupId = :category LIMIT 0, 15");
+                $stmt = $conn->prepare("SELECT SG.StockGroupID, S.StockItemName, S.RecommendedRetailPrice, S.StockItemID FROM stockitems S JOIN stockitemstockgroups G ON S.StockItemID = G.StockItemID JOIN stockgroups SG ON G.StockGroupID = SG.StockGroupID WHERE G.StockGroupId = :category LIMIT 15 OFFSET 0");
             } elseif (isset($_GET["limit"]) && !empty($_GET["limit"]) && $_GET["limit"] == 15) {
-                $stmt = $conn->prepare("SELECT SG.StockGroupID, S.StockItemName, S.RecommendedRetailPrice, S.StockItemID FROM stockitems S JOIN stockitemstockgroups G ON S.StockItemID = G.StockItemID JOIN stockgroups SG ON G.StockGroupID = SG.StockGroupID WHERE G.StockGroupId = :category LIMIT 15, 30");
+                $stmt = $conn->prepare("SELECT SG.StockGroupID, S.StockItemName, S.RecommendedRetailPrice, S.StockItemID FROM stockitems S JOIN stockitemstockgroups G ON S.StockItemID = G.StockItemID JOIN stockgroups SG ON G.StockGroupID = SG.StockGroupID WHERE G.StockGroupId = :category LIMIT 15 OFFSET 15");
             } elseif (isset($_GET["limit"]) && !empty($_GET["limit"]) && $_GET["limit"] == 30) {
-                $stmt = $conn->prepare("SELECT SG.StockGroupID, S.StockItemName, S.RecommendedRetailPrice, S.StockItemID FROM stockitems S JOIN stockitemstockgroups G ON S.StockItemID = G.StockItemID JOIN stockgroups SG ON G.StockGroupID = SG.StockGroupID WHERE G.StockGroupId = :category LIMIT 30, 45");
+                $stmt = $conn->prepare("SELECT SG.StockGroupID, S.StockItemName, S.RecommendedRetailPrice, S.StockItemID FROM stockitems S JOIN stockitemstockgroups G ON S.StockItemID = G.StockItemID JOIN stockgroups SG ON G.StockGroupID = SG.StockGroupID WHERE G.StockGroupId = :category LIMIT 15 OFFSET 30");
             } elseif (isset($_GET["limit"]) && !empty($_GET["limit"]) && $_GET["limit"] == 45) {
-                $stmt = $conn->prepare("SELECT SG.StockGroupID, S.StockItemName, S.RecommendedRetailPrice, S.StockItemID FROM stockitems S JOIN stockitemstockgroups G ON S.StockItemID = G.StockItemID JOIN stockgroups SG ON G.StockGroupID = SG.StockGroupID WHERE G.StockGroupId = :category LIMIT 45, 60");
+                $stmt = $conn->prepare("SELECT SG.StockGroupID, S.StockItemName, S.RecommendedRetailPrice, S.StockItemID FROM stockitems S JOIN stockitemstockgroups G ON S.StockItemID = G.StockItemID JOIN stockgroups SG ON G.StockGroupID = SG.StockGroupID WHERE G.StockGroupId = :category LIMIT 15 OFFSET 45");
             } elseif (isset($_GET["limit"]) && !empty($_GET["limit"]) && $_GET["limit"] == 60) {
-                $stmt = $conn->prepare("SELECT SG.StockGroupID, S.StockItemName, S.RecommendedRetailPrice, S.StockItemID FROM stockitems S JOIN stockitemstockgroups G ON S.StockItemID = G.StockItemID JOIN stockgroups SG ON G.StockGroupID = SG.StockGroupID WHERE G.StockGroupId = :category LIMIT 60, 75");
+                $stmt = $conn->prepare("SELECT SG.StockGroupID, S.StockItemName, S.RecommendedRetailPrice, S.StockItemID FROM stockitems S JOIN stockitemstockgroups G ON S.StockItemID = G.StockItemID JOIN stockgroups SG ON G.StockGroupID = SG.StockGroupID WHERE G.StockGroupId = :category LIMIT 15 OFFSET 60");
             } elseif (isset($_GET["limit"]) && !empty($_GET["limit"]) && $_GET["limit"] == 75) {
-                $stmt = $conn->prepare("SELECT SG.StockGroupID, S.StockItemName, S.RecommendedRetailPrice, S.StockItemID FROM stockitems S JOIN stockitemstockgroups G ON S.StockItemID = G.StockItemID JOIN stockgroups SG ON G.StockGroupID = SG.StockGroupID WHERE G.StockGroupId = :category LIMIT 75, 90");
+                $stmt = $conn->prepare("SELECT SG.StockGroupID, S.StockItemName, S.RecommendedRetailPrice, S.StockItemID FROM stockitems S JOIN stockitemstockgroups G ON S.StockItemID = G.StockItemID JOIN stockgroups SG ON G.StockGroupID = SG.StockGroupID WHERE G.StockGroupId = :category LIMIT 15 OFFSET 75");
             } elseif (isset($_GET["limit"]) && !empty($_GET["limit"]) && $_GET["limit"] == 90) {
-                $stmt = $conn->prepare("SELECT SG.StockGroupID, S.StockItemName, S.RecommendedRetailPrice, S.StockItemID FROM stockitems S JOIN stockitemstockgroups G ON S.StockItemID = G.StockItemID JOIN stockgroups SG ON G.StockGroupID = SG.StockGroupID WHERE G.StockGroupId = :category LIMIT 90, 105");
+                $stmt = $conn->prepare("SELECT SG.StockGroupID, S.StockItemName, S.RecommendedRetailPrice, S.StockItemID FROM stockitems S JOIN stockitemstockgroups G ON S.StockItemID = G.StockItemID JOIN stockgroups SG ON G.StockGroupID = SG.StockGroupID WHERE G.StockGroupId = :category LIMIT 15 OFFSET 90");
             } elseif (isset($_GET["limit"]) && !empty($_GET["limit"]) && $_GET["limit"] == 105) {
-                $stmt = $conn->prepare("SELECT SG.StockGroupID, S.StockItemName, S.RecommendedRetailPrice, S.StockItemID FROM stockitems S JOIN stockitemstockgroups G ON S.StockItemID = G.StockItemID JOIN stockgroups SG ON G.StockGroupID = SG.StockGroupID WHERE G.StockGroupId = :category LIMIT 105, 120");
-            } else {
-                $stmt = $conn->prepare("SELECT SG.StockGroupID, S.StockItemName, S.RecommendedRetailPrice, S.StockItemID FROM stockitems S JOIN stockitemstockgroups G ON S.StockItemID = G.StockItemID JOIN stockgroups SG ON G.StockGroupID = SG.StockGroupID WHERE G.StockGroupId = :category LIMIT 0, 15");
-
+                $stmt = $conn->prepare("SELECT SG.StockGroupID, S.StockItemName, S.RecommendedRetailPrice, S.StockItemID FROM stockitems S JOIN stockitemstockgroups G ON S.StockItemID = G.StockItemID JOIN stockgroups SG ON G.StockGroupID = SG.StockGroupID WHERE G.StockGroupId = :category LIMIT 15 OFFSET 105");
+            }
+            else {
+                $stmt = $conn->prepare("SELECT SG.StockGroupID, S.StockItemName, S.RecommendedRetailPrice, S.StockItemID FROM stockitems S JOIN stockitemstockgroups G ON S.StockItemID = G.StockItemID JOIN stockgroups SG ON G.StockGroupID = SG.StockGroupID WHERE G.StockGroupId = :category LIMIT 15 OFFSET 0");
             }
 
             $stmt->bindParam(":category", $category);
