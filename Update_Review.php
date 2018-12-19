71579509c9f235d2d93dbe58d0d115f0b485a4ce
jan-peter <jppoolen@gmail.com>
2018-12-19 14:27:44 +0100

    pushh

--- Update_Review.php
+++ Update_Review.php
@@ -1,11 +1,13 @@
 <?php
 
+include "inc/parts/db.php";
+
 $ProductID =  $_POST['ID'];
 $rating = $_POST['value'];
 $content = $_POST['content'];
 
-$sql = "INSERT INTO Review (StockItemID,Rating,Content) VALUES ('$ProductID','$rating', $content)";
+$sql = "INSERT INTO Review (StockItemID,Rating,Content) VALUES ($ProductID,$rating,'$content')";
 
-header ("refresh:1; url=productpage.php?productID=' . $ProductID . '");
+header ("refresh:1; url=productpage.php?productID= . $ProductID . ");
 
 ?>
